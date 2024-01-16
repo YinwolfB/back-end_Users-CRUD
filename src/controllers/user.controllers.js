ad1fea6ec3e7dd7570f401bb416a7664120e9c4b
YinwolfB <comandoxrp@hotmail.com>
2024-01-15 21:33:29 -0500

    Se termina el back-end de users-crud

---
+++ src/controllers/user.controllers.js
@@ -0,0 +1,60 @@
+const catchError = require('../utils/catchError');
+const User = require('../models/User');
+
+const getAll = catchError(async(req, res) => {
+    // Operaciones...
+    const user = await User.findAll()
+    return res.json(user)
+});
+
+//Create
+const create = catchError(async(req, res) => {
+    const {email, password, first_name, last_name, birthday} = req.body;
+    const user = await User.create({
+        email: email,
+        password: password,
+        first_name: first_name,
+        last_name: last_name,
+        birthday: birthday
+    });
+    return res.status(201).json(user);
+});
+
+//Get One
+
+const getOne = catchError(async(req, res) => {
+    const { id } = req.params;
+    const user = await User.findByPk(id);
+    return res.json(user);
+});
+
+//Delate 
+
+const remove = catchError(async(req, res) => {
+    const { id } = req.params;
+    await User.destroy({ where: { id: id } });
+    return res.sendStatus(204);
+});
+
+//Update
+
+const update = catchError(async(req, res) => {
+    const { id } = req.params;
+    const {email, password, first_name, last_name, birthday} = req.body;
+    const user = await User.update({
+        email: email,
+        password: password,
+        first_name: first_name,
+        last_name: last_name,
+        birthday: birthday
+    }, { where: { id: id }, returning: true });
+    return res.json(user);
+});
+
+module.exports = {
+    getAll,
+    create,
+    getOne,
+    remove,
+    update
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const userRouter = require('./user.router');
 const router = express.Router();
 
-// colocar las rutas aquí
+// colocar las rutas aqui
+router.use(userRouter);
 
 
 module.exports = router;
